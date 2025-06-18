1acf2494f4d05a04370f92f3ff476ef17d8174f2
Srijan Bhattacharjee <srijan@capitalnumbers.com>
2025-06-18 14:10:10 +0530

    fixed the env for braintree

--- src/controllers/payment-information.js
+++ src/controllers/payment-information.js
@@ -21,12 +21,15 @@ const getErrorMessage = (error) => {
   return "An unknown error occurred.";
 };
 
-// Create a new instance of the BraintreeGateway with the provided credentials
+const environment = process.env.BRAINTREE_ENVIRONMENT === 'Production'
+  ? braintree.Environment.Production
+  : braintree.Environment.Sandbox;
+
 const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: "h5wcnvynttcdssyn",
-  publicKey: "zhj2v8cwpv3692ys",
-  privateKey: "f951c0861c75fff83f212c421924aa45",
+  environment: environment,  // Use the resolved environment directly
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
 });
 
 exports.getClientToken = async (req, res) => {
